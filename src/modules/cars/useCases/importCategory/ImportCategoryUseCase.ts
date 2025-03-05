4ff53ce5fbf66b750f50de9c73fae20bd4e7d8c6
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-03-04 22:42:38 -0300

    promise

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,42 @@
+import { parse } from 'csv-parse';
+import fs from 'fs';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository.js';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse({
+        relax_column_count: true,
+      });
+
+      stream.pipe(parseFile);
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => resolve(categories))
+        .on('error', (err) => reject(err));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
+  }
+}
+
+export { ImportCategoryUseCase };
