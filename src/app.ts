8d2cb6bf47506f3fe80693563efbd8e98dcb028f
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-03-08 22:35:00 -0300

    tempDocker

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { router } from './routes/index.js';
 import swaggerUI from 'swagger-ui-express';
 import { readFile } from 'fs/promises';
+import { AppDataSource } from './database/index.js';
 
 async function startServer(): Promise<void> {
   const swaggerFile = JSON.parse(
     await readFile(new URL('./swagger.json', import.meta.url), 'utf-8'),
   );
-
+  AppDataSource.initialize();
   const app = express();
 
   app.use(express.json());
