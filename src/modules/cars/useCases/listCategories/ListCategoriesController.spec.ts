999b994cc45a60c0b1acd2b169f1eb6aa4a8a713
João <joaovictorcortabitart@gmail.com>
2025-07-24 22:45:57 -0300

    commit: Integrations Tests

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -2,17 +2,15 @@ import { app } from '@shared/infra/http/app';
 import { AppDataSource } from '@shared/infra/typeorm';
 import { hash } from 'bcrypt';
 import request from 'supertest';
-import { DataSource } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid'
 
-describe("Create Category Controller", () => {
+describe("List categories", () => {
     beforeAll(async () => {
         await AppDataSource.initialize()
         await AppDataSource.runMigrations()
 
         const password = await hash(process.env.ADMIN_PASSWORD!, 8);
         const id = uuidv4()
-
         await AppDataSource.query(
             `INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license) 
      VALUES ($1, $2, $3, $4, $5, $6, $7)`,
@@ -25,36 +23,24 @@ describe("Create Category Controller", () => {
         await AppDataSource.destroy()
     })
 
-    it("Should be able to create a categorie", async () => {
+    it("Should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: 'admin@rentx.com.br',
             password: process.env.ADMIN_PASSWORD
         })
         const { token } = responseToken.body
 
-        const response = await request(app).post("/categories").send({
+        await request(app).post("/categories").send({
             "name": "CategoriesSuperTestName",
             "description": "CategoriesSuperTestDescription"
         }).set({
             authorization: `Bearer ${token}`
         })
 
-        expect(response.status).toBe(201);
-    })
-    it('Should not be able to create a new category with name exists', async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: 'admin@rentx.com.br',
-            password: process.env.ADMIN_PASSWORD
-        })
-        const { token } = responseToken.body
-
-        const response = await request(app).post("/categories").send({
-            "name": "CategoriesSuperTestName",
-            "description": "CategoriesSuperTestDescription"
-        }).set({
-            authorization: `Bearer ${token}`
-        })
-        expect(response.status).toBe(400)
-
+        const response = await request(app).get("/categories")
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1)
+        expect(response.body[0]).toHaveProperty("id")
+        expect(response.body[0].name).toEqual("CategoriesSuperTestName")
     })
 })
